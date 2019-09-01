3994c34c63bec0f5f5b69cdb93aecc69269c36f0
renatodesouza <rlucas.rs2@gmail.com>
2019-09-01 13:02:18 -0300

    Subindo urls e views

--- outmed/app_outmed/views.py
+++ outmed/app_outmed/views.py
@@ -5,7 +5,6 @@ from .form import *
 from .models import *
 
 
-
 def index(request):
     return render(request, 'menu.html')
 
@@ -24,6 +23,24 @@ def criar_cliente(request):
         form.save()
         return redirect('listar_cliente')
     return render(request, 'clientes.html', {'form': form})
+
+def update_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    form = ClienteForm(request.POST or None, instance=cliente)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_cliente')
+    #OBS para o retorno abaixo deve ser tratado caso a atualização nao seja realizada
+    return render(request,'clientes.html', {'form': form, 'cliente': cliente})
+
+def delete_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('list_cliente')
+    return render(request, 'clientes.html', {'cliente': cliente})
     
 #def list_fornecedor(request):
 #    return render(request, 'fornecedor.html')
