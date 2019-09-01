311fbe80fb3985c40f330eaf90d5f4fcc01f2d3c
renatodesouza <rlucas.rs2@gmail.com>
2019-09-01 13:13:15 -0300

    Subindo arquivos __pycache__ e urls

--- outmed/outmed/urls.py
+++ outmed/outmed/urls.py
@@ -18,11 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('app_outmed.urls')),
-    #path('cadastro/', include('app_outmed.urls')),
-    path('cliente/', include('app_outmed.urls')),
-    path('listar_cliente/', include('app_outmed.urls')),
-    #path('fornecedor/', include('app_outmed.urls')),
-    #path('funcionario/', include('app_outmed.urls')),
     path('admin/', admin.site.urls),
 ]
 
