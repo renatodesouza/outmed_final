6d53a6918fb7b776894d254311344a5be96fd6c2
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2019-09-01 21:38:55 -0300

    Form de fornecedor

--- outmed/ app_outmed/form.py
+++ outmed/ app_outmed/form.py
@@ -6,5 +6,10 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['first_name', 'last_name', 'email', 'celular', 'fixo', 'cidade', 'bairro', 'rua', 'numero', 'cep']
+        
+class FornecedorForm(forms.ModelForm):
+    class Meta:
+        model = Fornecedor
+        fields = ['first_name', 'email', 'cnpj', 'email', 'cidade', 'bairro', 'rua', 'numero', 'cep']
 
       
