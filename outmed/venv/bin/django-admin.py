8884beb69afaff78a773eba0d84f84e854811369
gabelesss <gabriel.7rm@outlook.com>
2019-11-04 01:43:31 -0300

    Changes

---
+++ outmed/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/stark/Documentos/Dev/Python/Project/outmed_final/outmed/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
